104a25319956b5e885634460922879def2466847
Stephany Lam <36458967+waterFalt777@users.noreply.github.com>
2025-03-07 20:00:16 -0800

    implemented calculate_job_similarities

--- job_recommender_app.py
+++ job_recommender_app.py
@@ -24,8 +24,9 @@ st.sidebar.markdown("Scroll Down to See All Functionalities!")
 user_input = st.text_area("copy and paste your resume or linkedin here", '')
 
 user_input = str(user_input)
-user_input = re.sub('[^a-zA-Z0-9\.]', ' ', user_input)
-user_input = user_input.lower()
+user_input = re.sub('[^a-zA-Z0-9\.]', ' ', user_input)  
+#STEPH CHANGES: Renamed this user input var since we need the string version to use for calculate_job_similarities()
+first_user_input = user_input.lower()
 
 user_input = pd.Series(user_input)
 
@@ -51,7 +52,7 @@ def plot_user_probability():
     zippedDatalst = list(zip(data['jobs'],data['probability']))
     # Sorting by second element
     highest_prob_lst = sorted(zippedDatalst, key=lambda x: -x[1],)
-    print(highest_prob_lst)
+    #print(highest_prob_lst)
     
     #plotting bar graph
 
@@ -82,9 +83,19 @@ problst = plot_user_probability()
 
 #STEPH
 st.title("🔥💼TOP 5 MATCHING JOBS 💼🔥")
-top_profession = problst[0][0]
-joblst = pda.returnTop5Jobs(top_profession)#"data,analyst")
-st.write(joblst)
+#STEPH CHANGES:
+'''
+   1. Made If condition to check if user input is empty, if so, display message to enter resume
+   2. Called the calculate_job_similarities() function to get the top 5 matching jobs
+   3. Changed the calculate_job_similarities() to take in the string version of the user input
+'''
+if first_user_input == '':
+    st.write('Please enter your resume above to see your top 5 matching jobs!')
+else:
+    top_profession = problst[0][0]
+    joblst = pda.returnTop5Jobs(top_profession)#"data,analyst")
+    job_similarity_lst = pda.calculate_job_similarities(first_user_input, joblst) #changes to userinput as string, formerly as series
+    st.write(job_similarity_lst)
 
 #for SCATTER PLOT
 st.title('Representation Among Job Types')
